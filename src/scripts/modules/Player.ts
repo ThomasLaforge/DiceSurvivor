bce190a3bc14a540af2737346fb4b92edc966f00
Thomas Laforge <thomas.laforge.38@gmail.com>
2017-04-11 18:20:32 +0200

    upgrade player

--- src/scripts/modules/Player.ts
+++ src/scripts/modules/Player.ts
@@ -1,13 +1,54 @@
 import { DiceCollection } from './DiceCollection'
+import { Dice } from './Dice'
+import { Resource } from './Resource'
+import { Stuff } from './Stuff'
+import { Inventary } from './Inventary'
+import { HUNGER__DEFAULT_LOSE, HUNGER__DEFAULT_WIN, HUNGER__MIN, HUNGER__MAX, HEALTH__DEFAULT_LOSE, HEALTH__DEFAULT_WIN, HEALTH__MIN, HEALTH__MAX, HUNGER__DEFAULT, HEALTH__DEFAULT } from './Configuration'
 
 export class Player {
 
     private _pseudo: string;
     private _diceCollection: DiceCollection;
+	private _health: number;
+	private _hunger: number;
+	private _inventary: Inventary;
 
-	constructor(pseudo: string, diceCollection: DiceCollection) {
+	constructor(pseudo: string, diceCollection: DiceCollection, health: number = HEALTH__DEFAULT, hunger: number = HUNGER__DEFAULT) {
 		this._pseudo = pseudo;
 		this._diceCollection = diceCollection;
+	}
+
+	winHealth(healthWon: number = HEALTH__DEFAULT_WIN){
+		this.health += healthWon;
+		if(this.health > HEALTH__MAX) {
+			this.health = HEALTH__MAX
+		}
+	}
+	loseHealth(healthLost: number = HEALTH__DEFAULT_LOSE){
+		this.health -= healthLost;
+		if(this.health < HEALTH__MIN) {
+			this.health = HEALTH__MIN
+		}
+	}
+	winHunger(hungerWon: number = HUNGER__DEFAULT_WIN){
+		this.hunger += hungerWon;
+		if(this.hunger > HUNGER__MAX) {
+			this.hunger = HUNGER__MAX
+		}
+	}
+	loseHunger(hungerLost: number = HUNGER__DEFAULT_LOSE){
+		this.hunger -= hungerLost;
+		if(this.hunger < HUNGER__MIN) {
+			this.hunger = HUNGER__MIN
+		}
+	}
+
+	getNewDice(){
+		this.diceCollection.addNewDice();
+	}
+
+	loseDice(dice?: Dice){
+		this.diceCollection.removeDice(dice)
 	}
 
 	public get pseudo(): string {
@@ -21,6 +62,18 @@ export class Player {
 	}
 	public set diceCollection(value: DiceCollection) {
 		this._diceCollection = value;
+	}
+	public get health(): number {
+		return this._health;
+	}
+	public set health(value: number) {
+		this._health = value;
+	}
+	public get hunger(): number {
+		return this._hunger;
+	}
+	public set hunger(value: number) {
+		this._hunger = value;
 	}
     
 
