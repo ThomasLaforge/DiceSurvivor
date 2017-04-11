112987d6d91106e2de541b30bd8e85211aee3c57
Thomas Laforge <thomas.laforge.38@gmail.com>
2017-04-11 08:53:33 +0200

    add player on game

--- src/scripts/modules/Game.ts
+++ src/scripts/modules/Game.ts
@@ -1,9 +1,12 @@
 // imports
     import { Dice }          from './Dice';
+    import { Player }          from './Player';
     import {  }              from './DiceSurvivor'
 // -------
 
 class Game {
+
+	private _player: Player; 
 
 	constructor() {}
 	
